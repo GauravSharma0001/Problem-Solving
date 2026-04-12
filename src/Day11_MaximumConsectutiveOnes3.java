aab1d3e568c146c19c3cef791339a0c347e53dd4
GauravSharma0001 <gsharma201099@gmail.com>
2026-04-12 23:56:06 +0530

    Day 10 problem is "Max Consecutive Ones".

---
+++ src/Day11_MaximumConsectutiveOnes3.java
@@ -0,0 +1,25 @@
+//1004. Max Consecutive Ones III
+//Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+public class Day11_MaximumConsectutiveOnes3 {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+        int k = 2;
+        int res= longestOnes(nums,k);
+        System.out.println(res);
+    }
+    public static int longestOnes(int[] nums, int k) {
+        int sum=0;
+        int l=0;
+        int ans=0;
+        for(int r=0; r<=nums.length-1;r++){
+            sum+=nums[r];
+            while(sum+k<r-l+1){
+                sum-=nums[l];
+                l++;
+            }
+            ans=Math.max(ans,r-l+1);
+        }
+        return ans;
+    }
+}
